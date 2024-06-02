cc6fd360474a4d00b63456475fafa42cc6338b9a
Crixpsitos <162711947+Crixpsitos@users.noreply.github.com>
2024-06-02 17:17:48 -0500

    add the auth in the project

--- apps/auth/urls.py
+++ apps/auth/urls.py
@@ -4,5 +4,6 @@ from .views import *
 
 urlpatterns = [
     path('register/', register, name='register'),
-    path('login/', login, name='login')
+    path('login/', login, name='login'),
+    path('logout/', logout, name='logout')
 ]
--- apps/auth/views.py
+++ apps/auth/views.py
@@ -1,16 +1,19 @@
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout , login as auth_login
 from django.contrib.auth import authenticate
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import Group
 
 def login(request):
-    return render(request, 'auth/login.html')
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+           auth_login(request, form.get_user())
+           return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'auth/login.html', {'form': form})
 
-def create_groups(sender, **kwargs):
-    Group.objects.get_or_create(name='student')
-    Group.objects.get_or_create(name='teacher')
-    Group.objects.get_or_create(name='admin')
 
 def register(request):
     if request.method == 'POST':
@@ -21,3 +24,14 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'auth/register.html', {'form': form})
+
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect('home')
+
+def create_groups(sender, **kwargs):
+    Group.objects.get_or_create(name='student')
+    Group.objects.get_or_create(name='teacher')
+    Group.objects.get_or_create(name='admin')
